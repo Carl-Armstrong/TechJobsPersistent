3ae144de99148afda2ee4d11e26c8227e450838d
Carl Armstrong <carljarms@gmail.com>
2021-12-06 19:56:13 -0600

    'Add Employer function implemented'

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -24,7 +24,7 @@ namespace TechJobsPersistent.Controllers
         public IActionResult Index()
         {
             List<Employer> employers = context.Employers.ToList();
-            return View();
+            return View(employers);
         }
 
         public IActionResult Add()
--- TechJobsPersistent/ViewModels/AddJobViewModel.cs
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace TechJobsPersistent.ViewModels
     public class AddJobViewModel
     {
         public string Name { get; set; }
-        public int id { get; set; }
-        public List<Employer> SelectListItem { get; set; }
+        public int EmployerId { get; set; }
+        public List<SelectListItem> Employers { get; set; }
     }
 }
